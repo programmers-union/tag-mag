ab7937b3646833b955ee2b569ea886aa23552162
p11 <github@programmersunion.in>
2025-05-14 18:22:36 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
         <h2 className="section-subtitle   py-5">Latest From Business</h2>
       </div>
 
-         <div className=" mx-5 row border-black border-bottom">
+      <div className=" mx-5 row border-black border-bottom">
         <div className="col-lg-6">
           <ArticleCard
             category="Business"
@@ -108,13 +108,114 @@ export default function Home() {
             imageAlt="Skincare routine demonstration"
             title="Plug into the Future: Exploring the World of Electric Vehicles"
             href="/articles/skincare-secrets"
-           imageWidth="600px"
+            imageWidth="600px"
             imageHeight="400px"
           />
         </div>
-
       </div>
-       
+      <div className="row justify-content-center border-bottom border-black  col-12 col-md-10 col-lg-12 text-center mx-auto">
+        <h2 className="section-subtitle   py-5">Latest From Technology</h2>
+      </div>
+      <div className=" mx-5 row border-black border-bottom">
+        <div className="col-lg-4">
+          <ArticleCard
+            category="Technology"
+            readTime="3 Min Read"
+            imageUrl="https://cdn.prod.website-files.com/65706a428903abc2db294774/65bb7b10aef080ce3f9423b8_pexels-cottonbro-studio-6153354-p-500.jpg"
+            imageAlt="Skincare routine demonstration"
+            title="Cognitive Chronicles: Exploring the World of Artificial Intelligence"
+            href="/articles/skincare-secrets"
+            imageWidth="450px"
+            imageHeight="300px"
+          />
+        </div>
+        <div className="col-lg-4">
+          <ArticleCard
+            category="Technology"
+            readTime="4 Min Read"
+            imageUrl="https://cdn.prod.website-files.com/65706a428903abc2db294774/65bb7ce5cd2756c48e18be82_pexels-thisisengineering-3861969-p-500.jpg"
+            imageAlt="Skincare routine demonstration"
+            title="Future Tech Files: Dive into Tomorrow's Innovations Today"
+            href="/articles/skincare-secrets"
+          />
+        </div>
+        <div className="col-lg-4">
+          <ArticleCard
+            category="Technology"
+            readTime="3 Min Read"
+            imageUrl="https://cdn.prod.website-files.com/65706a428903abc2db294774/65ba7b99b362bb03d11dbd88_pexels-michelangelo-buonarroti-8728380-p-500.jpg"
+            imageAlt="Skincare routine demonstration"
+            title="Navigating the Future: Cutting-Edge Technologies Redefining Tomorrow"
+            href="/articles/skincare-secrets"
+          />
+        </div>
+      </div>
+      <div className="row justify-content-center border-bottom border-black  col-12 col-md-10 col-lg-12 text-center mx-auto">
+        <h2 className="section-subtitle   py-5">New From Entertainment</h2>
+      </div>
+      <div className=" mx-5 row ">
+        <div className="col-lg-4">
+          <ArticleCard
+            category="Entertainment"
+            readTime="4 Min Read"
+            imageUrl="https://cdn.prod.website-files.com/65706a428903abc2db294774/65b7d99487ba0bab5634ede1_pexels-louis-1200450-p-500.jpg"
+            imageAlt="Skincare routine demonstration"
+            title="Cinephile Chronicles: The Latest in Cinematic Wonders"
+            href="/articles/skincare-secrets"
+            imageWidth="450px"
+            imageHeight="300px"
+          />
+        </div>
+        <div className="col-lg-4">
+          <ArticleCard
+            category="Entertainment"
+            readTime="4 Min Read"
+            imageUrl="https://cdn.prod.website-files.com/65706a428903abc2db294774/65b7bfcb60f091d7bd50ed9e_pexels-jo-kassis-6461411-p-500.jpg"
+            imageAlt="Skincare routine demonstration"
+            title="The Evolution of Dance: From TikTok to Mainstream Culture"
+            href="/articles/skincare-secrets"
+          />
+        </div>
+        <div className="col-lg-4">
+          <ArticleCard
+            category="Entertainment"
+            readTime="4 Min Read"
+            imageUrl="https://cdn.prod.website-files.com/65706a428903abc2db294774/65ba4ccd05da28331cf8b7e1_pexels-suzy-hazelwood-3248644-p-500.jpg  "
+            imageAlt="Skincare routine demonstration"
+            title="Reading Room Revelations: The Latest in Literature"
+            href="/articles/skincare-secrets"
+          />
+        </div>
+      </div>
+            <div className="row justify-content-center border-bottom border-black  col-12 col-md-10 col-lg-12 text-center mx-auto">
+        <h2 className="section-subtitle   py-5">New From Lifestyle</h2>
+      </div>
+            <div className=" mx-5 row border-black border-bottom">
+        <div className="col-lg-6">
+          <ArticleCard
+            category="Lifestyle"
+            readTime="2 Min Read"
+            imageUrl="https://cdn.prod.website-files.com/65706a428903abc2db294774/65ba6fe5e8b4c5d5d4520616_pexels-pus%CC%A6cas%CC%A6-adryan-13051215-p-800.jpg"
+            imageAlt="Skincare routine demonstration"
+            title="Minimalist Haven: Embracing Simplicity in Your Living Space"
+            href="/articles/skincare-secrets"
+            imageWidth="600px"
+            imageHeight="400px"
+          />
+        </div>
+        <div className="col-lg-6">
+          <ArticleCard
+            category="Lifestyle"
+            readTime="3 Min Read"
+            imageUrl="https://cdn.prod.website-files.com/65706a428903abc2db294774/65ba6b4ce56d392df5886fed_pexels-pixabay-416747-p-800.jpg"
+            imageAlt="Skincare routine demonstration"
+            title="Fitness Fusion: Balancing Physical and Mental Health"
+            href="/articles/skincare-secrets"
+            imageWidth="600px"
+            imageHeight="400px"
+          />
+        </div>
+      </div>
     </div>
   );
 }

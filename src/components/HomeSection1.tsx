52d6e6af7b41d1109158b551f7fd60c6899e7050
p11 <github@programmersunion.in>
2025-06-24 12:02:33 +0530

    updated component

--- src/components/HomeSection1.tsx
+++ src/components/HomeSection1.tsx
@@ -2,7 +2,7 @@
   return (
     <div className="section-header ps-2  p-3">
       <div className="row justify-content-center border-bottom border-black  col-12 col-md-10 col-lg-12 text-center mx-auto">
-        <h1  className="section-title border-black border-bottom py-3 ">We Are Tagmag</h1>
+        <h1  className="section-title border-black border-bottom py-3 ">We Are  Tagmag</h1>
 
           <h2 className="section-subtitle   py-5">Curated Articles</h2>
         
